983317d231aeb62b47dfffdbf9c872efea7ad9fa
Francis Kiarie <fkiarie100@gmail.com>
2025-03-22 11:24:35 +0300

    Add scroll-based header activation functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 // Grab elements
-
-//Nav styles on scroll
-
+const selectElement = selector =>{
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Please check your selector ${selector}`);
+};
+//Nav styles on scrolla
+const scrolHeader = () =>{
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }else{
+        headerElement.classList.remove('activated');
+    }
+}
+window.addEventListener('scroll', scrolHeader);
 // Open menu & search pop-up
 
 // Open/Close search form popup
